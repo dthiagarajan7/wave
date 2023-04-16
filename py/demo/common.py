fd2effc85f9fb3e578ff51678e27ad9753c6cb0b
dthiagarajan7 <84405827+dthiagarajan7@users.noreply.github.com>
2023-04-16 06:57:54 -0400

    feat: Render dashboard demo app links directly within header #1867

--- py/demo/common.py
+++ py/demo/common.py
@@ -9,27 +9,3 @@ global_nav = [
     ui.tab(name='#dashboards/mint', label='Mint'),
     ui.tab(name='#dashboards/purple', label='Purple'),
 ]
-
-'''global_nav = [
-    ui.tabs(name='Dashboards', items=[
-        ui.tab(name='#dashboards/red', label='Red'),
-        ui.tab(name='#dashboards/blue', label='Blue'),
-        ui.tab(name='#dashboards/orange', label='Orange'),
-        ui.tab(name='#dashboards/cyan', label='Cyan'),
-        ui.tab(name='#dashboards/grey', label='Grey'),
-        ui.tab(name='#dashboards/mint', label='Mint'),
-        ui.tab(name='#dashboards/purple', label='Purple'),
-    ]),
-]'''
-
-'''global_nav = [
-    ui.nav_group('Dashboards', items=[
-        ui.nav_item(name='#dashboards/red', label='Red'),
-        ui.nav_item(name='#dashboards/blue', label='Blue'),
-        ui.nav_item(name='#dashboards/orange', label='Orange'),
-        ui.nav_item(name='#dashboards/cyan', label='Cyan'),
-        ui.nav_item(name='#dashboards/grey', label='Grey'),
-        ui.nav_item(name='#dashboards/mint', label='Mint'),
-        ui.nav_item(name='#dashboards/purple', label='Purple'),
-    ]),
-]'''
